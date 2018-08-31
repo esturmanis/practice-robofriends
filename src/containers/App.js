95d775655172df5fb9372150a19ec53adf1c2949
ESturmanis-Laptop-Win <edgars.sturmanis@gmail.com>
2018-08-30 23:40:00 -0700

    added async redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,43 +4,37 @@ import { connect } from 'react-redux';
 import SearchBox from '../components/SearchBox'; 
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
     }
 }
 
 // https://jsonplaceholder.typicode.com/users
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            robots: []
-        }
-    }
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({ robots: users }))
-            .catch(error => console.error(error));
+        this.props.onRequestRobots();
     }
 
     render() {
-        const { robots } = this.state;
-        const { searchField, onSearchChange } = this.props;
+        const { searchField, onSearchChange, robots, isPending } = this.props;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
-        if(robots.length === 0) {
+        if(isPending) {
             return <h1>Loading</h1>
         } else {
             return (
